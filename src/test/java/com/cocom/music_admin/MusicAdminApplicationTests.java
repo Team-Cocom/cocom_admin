ad17dee37e7e8c5d5a0726518f2567f195a5b351
yuno <chldh1533@gamil.com>
2022-07-11 10:49:14 +0900

    기본 정보 설정

---
+++ src/test/java/com/cocom/music_admin/MusicAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocom.music_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
